cdc93da114b41ec343864452c6f78065817c6081
Jameson <james.bradlee@telenor.no>
2023-11-11 17:03:02 +0100

    fix: zoneId on datetimeformatter

--- src/main/kotlin/no/telenor/kt/env/parsers/DateTimeFormatterParser.kt
+++ src/main/kotlin/no/telenor/kt/env/parsers/DateTimeFormatterParser.kt
@@ -3,10 +3,11 @@
 package no.telenor.kt.env.parsers
 
 import no.telenor.kt.env.Parser
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import kotlin.reflect.KType
 
 class DateTimeFormatterParser : Parser {
 	fun parseDateTimeFormatter(type: KType, name: String, value: String): DateTimeFormatter =
-		DateTimeFormatter.ofPattern(value)
+		DateTimeFormatter.ofPattern(value).withZone(ZoneId.systemDefault())
 }
